c0dd9fafe7f9a94b3cacfe9129bf0bae2e9da6c8
Mert <merttunc20@gmail.com>
2022-01-13 05:28:57 +0300

    first commit

---
+++ DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,42 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        Context _context = new Context();
+        public void Delete(T t)
+        {
+            _context.Remove(t);
+            _context.SaveChanges();
+        }
+
+        public T GetById(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public List<T> GetListAll()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public void Insert(T t)
+        {
+            _context.Add(t);
+            _context.SaveChanges();
+        }
+
+        public void Update(T t)
+        {
+            _context.Update(t);
+            _context.SaveChanges();
+        }
+    }
+}
